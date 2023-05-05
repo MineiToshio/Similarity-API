33e030b6862fc382542cb3c0e698bc0628ca9c2d
Toshio Minei <sminei10@gmail.com>
2023-05-05 01:38:04 -0500

    feat: implemented navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Providers from "@/components/ui/Providers";
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
       className={cn("bg-white text-slate-900 antialiased", inter.className)}
     >
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <Toaster position="bottom-right" />
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
+        </Providers>
 
         {/* Allow for more height on mobile devices */}
         <div className="h-40 md:hidden" />
