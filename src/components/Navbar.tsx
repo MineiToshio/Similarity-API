f90d2045f4053644e045c760220a86e637256649
Toshio Minei <sminei10@gmail.com>
2023-05-11 02:23:44 -0500

    fix: added missing text in dashboard menu link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,9 @@ const Navbar = async () => {
               <Link
                 className={buttonVariants({ variant: "ghost" })}
                 href="/dashboard"
-              ></Link>
+              >
+                Dashboard
+              </Link>
               <SignOutButton />
             </>
           ) : (
